0eb22c21f92e0796a398f95a049c57e02e265248
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2016-11-01 12:39:20 +0200

    Modify the pdf build task
    Add Dealer Token section to the build

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp'),
 	concat = require('gulp-concat')
 
 gulp.task('pdf', () => {
-	gulp.src(['./rules/index.md', './rules/components/index.md', './rules/components/zone/index.md', './rules/components/dice_roll/index.md', './rules/components/card/index.md', './rules/components/card/class/index.md', './rules/components/card/class/event/index.md'])
+	gulp.src(['./rules/index.md', './rules/components/index.md', './rules/components/dealer_token/index.md', './rules/components/zone/index.md', './rules/components/dice_roll/index.md', './rules/components/card/index.md', './rules/components/card/class/index.md', './rules/components/card/class/event/index.md'])
 		.pipe(concat('dml.md'))
 		.pipe(md2pdf())
 		.pipe(gulp.dest('build'))
