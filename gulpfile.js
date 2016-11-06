cfd4320afe9dff3be8e96a627f97a4f828eb6e33
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2016-11-06 15:41:51 +0200

    Modify build task
    Add missing sections and reorder the files.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp'),
 	concat = require('gulp-concat')
 
 gulp.task('pdf', () => {
-	gulp.src(['./rules/index.md', './rules/components/index.md', './rules/components/dealer_token/index.md', './rules/components/zone/index.md', './rules/components/dice_roll/index.md', './rules/components/card/index.md', './rules/components/card/class/index.md', './rules/components/card/class/event/index.md'])
+	gulp.src(['./rules/index.md', './rules/components/index.md', './rules/components/dealer_token/index.md', './rules/components/zone/index.md', './rules/components/dice_roll/index.md', './rules/components/victory_condition/index.md', './rules/components/card/class/index.md', './rules/components/card/class/event/index.md', './rules/components/card/index.md', './rules/gameplay/index.md', './rules/gameplay/setup/index.md', './rules/gameplay/setup/index.md', './rules/gameplay/game/index.md', './rules/gameplay/game/buying/index.md', './rules/gameplay/end/index.md', './rules/gameplay/modes/index.md'])
 		.pipe(concat('dml.md'))
 		.pipe(md2pdf())
 		.pipe(gulp.dest('build'))
